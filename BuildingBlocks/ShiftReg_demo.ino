9c98e0baffc61f33dff57b3f0f4238779316847f
David Nicholai <46093989+DNicholai@users.noreply.github.com>
2025-11-08 10:28:25 +0100

    Update ShiftReg_demo.ino
    Demo working with a more versatile library

--- BuildingBlocks/ShiftReg_demo.ino
+++ BuildingBlocks/ShiftReg_demo.ino
@@ -1,56 +1,77 @@
-#include <Shifty.h>
-#include <Streaming.h>  // Library for changing the syntax for Serial outs to make it more ergonomic (Serial << "output" << endl; )
+/*
+  ShiftRegister74HC595 - Library for simplified control of 74HC595 shift registers.
+  Developed and maintained by Timo Denk and contributers, since Nov 2014.
+  Additional information is available at https://timodenk.com/blog/shift-register-arduino-library/
+  Released into the public domain.
+*/
+
+#include <ShiftRegister74HC595.h>
+#include <Streaming.h>  // Library for changing the syntax for Serial outs to make it more ergonomic (Serial << "text" << endl; )
 
 
-// Declare the shift register
-Shifty shift; 
+// ST_CP pin 
+const int latchPin = 18;
+// SH_CP pin 
+const int clockPin = 20;
+// DS pin 
+const int dataPin = 19;
+
+// create a global shift register object
+// parameters: <number of shift registers> (data pin, clock pin, latch pin)
+ShiftRegister74HC595<1> sr(dataPin, clockPin, latchPin);  // Crucial ) the values cannot be set numerically in this line, but must be send as variables
+ 
 
 
-void setup() {
-  // Set the number of bits you have (multiples of 8)
-  shift.setBitCount(8);
 
-  // Set the clock, data, and latch pins you are using
-  // This also sets the pinMode for these pins
-  shift.setPins(19, 20, 18); 
+void setup() { 
   Serial.begin(115200);
   Serial << "Serial started in setup loop -- " << endl ;
 }
-  
-
 
 void loop() {
-  // writeBit works just like digitalWrite
-  shift.writeBit(1, HIGH);
-  Serial << "1 high" << endl; 
-  delay(1500);
-  shift.writeBit(3, HIGH);
-  Serial << "3 high" << endl;
-  delay(1500);
-  shift.writeBit(1, LOW);
-  Serial << "1 low" << endl; 
-  delay(1500);
-  shift.writeBit(3, LOW);
-  Serial << "3 low" << endl; 
-  
-  delay(1500);
 
-    
-  shift.writeBit(5, HIGH);
-  delay(1500);
-  shift.writeBit(0, HIGH);
-  delay(1500);
-  shift.writeBit(0, LOW);
-  shift.writeBit(1, LOW);
+  // setting all pins at the same time to either HIGH or LOW
+  sr.setAllHigh(); // set all pins HIGH
+  Serial << "Set all LEDs high" << endl; 
+  delay(2000);
   
-  shift.writeBit(2, LOW);
-  shift.writeBit(3, LOW);
-    
-  shift.writeBit(4, LOW);
-    
-  shift.writeBit(5, LOW);
-  delay(1500);
+  sr.setAllLow(); // set all pins LOW
+  Serial << "Set all LEDs low" << endl; 
+  delay(2000); 
   
- 
- 
+
+  // setting single pins0
+  Serial << "Interate through the pins" << endl; 
+  for (int i = 0; i < 8; i++) {
+    sr.setAllLow(); // set all pins LOW
+    sr.set(i, HIGH); // set single pin HIGH
+    delay(100); 
+  }
+    // setting single pins0
+  Serial << "Interate through the pins QUICKLY" << endl; 
+  for (int i = 0; i < 8; i++) {
+    sr.setAllLow(); // set all pins LOW
+    sr.set(i, HIGH); // set single pin HIGH
+    delay(20); 
+  }
+  
+  // set all pins at once
+  Serial << "Set the pin values to B10111111" << endl; 
+  uint8_t pinValues[] = { B10111111 }; 
+  sr.setAll(pinValues);
+  delay(4000);
+
+  
+  // read pin (zero based, i.e. 6th pin)
+  uint8_t stateOfPin5 = sr.get(5);
+  sr.set(6, stateOfPin5);
+  delay(1000);
+
+
+  // set pins without immediate update
+  sr.setNoUpdate(0, HIGH);
+  sr.setNoUpdate(1, LOW);
+  // at this point of time, pin 0 and 1 did not change yet
+  sr.updateRegisters(); // update the pins to the set values
+
 }
